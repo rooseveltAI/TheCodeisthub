778551c19abeff2d0e7e3ae88f2568e270efadf7
Roosevelt Innocent <howdy.roosevelt@gmail.com>
2019-06-20 00:23:03 +0100

    Initial commit

--- src/pages/works.js
+++ src/pages/works.js
@@ -4,7 +4,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { Works } from "../utils"
+import { Works } from "../../utils"
 
 import WorksSection from "../components/Works/worksSection"
 
