e9f226a0b674fd88471eda532756ac7090065331
Roosevelt Innocent <howdy.roosevelt@gmail.com>
2019-12-27 09:05:17 +0100

    Coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,63 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HomeHeader, Services, Works, Contact } from "../utils"
-import AboutIntro from "../components/About/AboutIntro"
-import ServicesSection from "../components/Service/ServicesSection"
-import WorkSection from "../components/Works/workPage"
-import ContactContainer from "../components/Contact/contactContainer"
+// import { HomeHeader, Services, Works, Contact } from "../utils"
+// import AboutIntro from "../components/About/AboutIntro"
+// import ServicesSection from "../components/Service/ServicesSection"
+// import WorkSection from "../components/Works/workPage"
+// import ContactContainer from "../components/Contact/contactContainer"
 
 // import About from "../components/HomePageComponents/About"
 
 const IndexPage = () => (
-  <Layout>
+  <div className="container">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <HomeHeader>
+
+    <div className="come--later__container">
+      <h1>Hello There!</h1>
+      <p>My site is currently under maintanance. Please check back later!</p>
+      <p>For the mean time you can connect with me on</p>
+      <a
+        href="https://twitter.com/CodeUXI"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      <a
+        href="https://www.linkedin.com/in/roosevelt-innocent/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+      <a
+        href="https://dribbble.com/CodeUXI"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Dribbble
+      </a>
+      <a
+        href="https://github.com/CodeUXI"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      <p>Or email me at</p>
+      <a
+        href="mailto:howdy.roosevelt@gmail.com"
+        target="_top"
+        rel="noopener noreferrer"
+      >
+        howdy.roosevelt@gmail.com
+      </a>
+    </div>
+
+    {/* <HomeHeader>
       <AboutIntro />
     </HomeHeader>
     <Services>
@@ -27,8 +70,8 @@ const IndexPage = () => (
 
     <Contact>
       <ContactContainer />
-    </Contact>
-  </Layout>
+    </Contact> */}
+  </div>
 )
 
 export default IndexPage
